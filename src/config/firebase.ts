0fdd6f63dfc19ec46627bacbd43641880bcdfe38
mikiIDE <kinoko.miki@gmail.com>
2025-03-10 15:12:40 +0900

    modified HomeScreen

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import {
   API_KEY,
   AUTH_DOMAIN,
@@ -23,5 +24,6 @@ const firebaseConfig = {
 // Firebaseの初期化
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+export { app, auth, db };
