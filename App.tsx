8a26f9f5e857fad0da6773dc395dcdd5c3e36e74
mikiIDE <kinoko.miki@gmail.com>
2025-03-11 15:11:03 +0900

    complete for UserToken

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+// App.tsx
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import RootNavigator from "./src/navigation/RootNavigator";
