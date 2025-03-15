662905842a0bac9ebd2e6f9e7725c5858f8cdfd5
mikiIDE <kinoko.miki@gmail.com>
2025-03-16 00:51:38 +0900

    fix:development build gesture-handler error

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 // index.ts
+import "react-native-gesture-handler";
+import { AppRegistry } from "react-native";
 import { registerRootComponent } from "expo";
-
 import App from "./App";
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// 明示的にAppRegistryに登録
+AppRegistry.registerComponent("main", () => App);
+
+// Expoのregisterを維持
 registerRootComponent(App);
