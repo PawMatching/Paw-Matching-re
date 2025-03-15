38e3cee5094ad8d04ede7a9bfb991680144781f9
mikiIDE <kinoko.miki@gmail.com>
2025-03-15 20:06:35 +0900

    fix:about reanimated

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      "react-native-reanimated/plugin", // これを最初にする
       [
         "module:react-native-dotenv",
         {
@@ -14,7 +15,6 @@ module.exports = function (api) {
           allowUndefined: true,
         },
       ],
-      "react-native-reanimated/plugin",
       [
         "module-resolver",
         {
